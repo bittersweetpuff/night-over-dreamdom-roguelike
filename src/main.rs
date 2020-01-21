0e963cc4481e97ea145ad687cdb42ef6264332af
BittersweetPuff <biszkoptex@gmail.com>
2020-01-21 16:11:05 +0100

    Chapter 1 of tutorial compleated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+rltk::add_wasm_support!();
+use rltk::{Rltk, GameState, Console};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk){
+        ctx.cls();
+        ctx.print(1, 1, "Short time ago in the world that should not exist...");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let context = Rltk::init_simple8x8(80, 50, "Night over Dreamdom", "resources");
+    let gs = State{ };
+    rltk::main_loop(context, gs);
 }
